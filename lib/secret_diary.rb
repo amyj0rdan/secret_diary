409733ab1d6a81b79138cf4aac443a062628e084
Student <student@Makerss-MacBook-Pro.local>
2019-03-08 10:26:44 +0000

    Return 'lock' for #lock method

--- lib/secret_diary.rb
+++ lib/secret_diary.rb
@@ -1,6 +1,7 @@
 class SecretDiary
 
   def lock
+    "Locked"
   end
 
 end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,7 +1,14 @@
 require 'secret_diary'
 
 describe SecretDiary do
+  describe '#lock' do
 
-  it { is_expected.to respond_to :lock }
+    it { is_expected.to respond_to :lock }
 
+    it 'should be lock the diary' do
+      my_diary = SecretDiary.new
+      expect(my_diary.lock).to eq "Locked"
+    end
+
+  end
 end
